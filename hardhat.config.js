e5da68d5240a027c213fe0c05aadad73d66401f2
Wattsy <wattsyart@protonmail.com>
2022-06-11 16:40:31 -0400

    add test support libraries

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -6,6 +9,31 @@ require("@nomiclabs/hardhat-waffle");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.13",
+ module.exports = {
+  networks: {
+    hardhat: {
+      gas: "auto",
+      blockGasLimit: 900_000_000_000,
+      initialBaseFeePerGas: 0,
+      timeout: 43200000
+    }
+  },
+  solidity: {
+    version: "0.8.13",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  },
+  mocha: {
+    timeout: 2000000000
+  },
+  gasReporter: {
+    currency: 'ETH',
+    gasPriceApi: 'https://api.etherscan.io/api?module=proxy&action=eth_gasPrice',
+    coinmarketcap: 'COINMARKETCAP_API_KEY'
+  }
 };
+
