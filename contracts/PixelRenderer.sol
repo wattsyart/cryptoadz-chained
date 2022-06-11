b63b2a8ebe2c30195b3dc626ad5b5ced7f016246
Wattsy <wattsyart@protonmail.com>
2022-06-11 19:01:29 -0400

    add alpha blending to pixel rendering

--- contracts/PixelRenderer.sol
+++ contracts/PixelRenderer.sol
@@ -119,7 +119,7 @@ library PixelRenderer {
         uint32 color
     ) private pure {
         uint32 p = uint32(int16(frame.width) * y + x);
-        frame.buffer[p] = color;
+        frame.buffer[p] = blend(frame.buffer[p], color);
     }
 
     function line(GIFEncoder.GIFFrame memory frame, Line2D memory f)
@@ -144,7 +144,7 @@ library PixelRenderer {
                 y0 >= int32(0)
             ) {
                 uint256 p = uint256(int16(frame.width) * y0 + x0);
-                frame.buffer[p] = f.color;
+                frame.buffer[p] = blend(frame.buffer[p], f.color);
             }
 
             if (x0 == x1 && y0 == y1) break;
@@ -158,6 +158,32 @@ library PixelRenderer {
                 y0 += y0 < y1 ? int8(1) : -1;
             }
         }
+    }
+
+    function blend(uint32 bg, uint32 fg) private pure returns (uint32) {
+        uint32 r1 = bg >> 16;
+        uint32 g1 = bg >> 8;
+        uint32 b1 = bg;
+        
+        uint32 a2 = fg >> 24;
+        uint32 r2 = fg >> 16;
+        uint32 g2 = fg >> 8;
+        uint32 b2 = fg;
+        
+        uint32 alpha = (a2 & 0xFF) + 1;
+        uint32 inverseAlpha = 257 - alpha;
+
+        uint32 r = (alpha * (r2 & 0xFF) + inverseAlpha * (r1 & 0xFF)) >> 8;
+        uint32 g = (alpha * (g2 & 0xFF) + inverseAlpha * (g1 & 0xFF)) >> 8;
+        uint32 b = (alpha * (b2 & 0xFF) + inverseAlpha * (b1 & 0xFF)) >> 8;
+
+        uint32 rgb = 0;
+        rgb |= uint32(0xFF) << 24;
+        rgb |= r << 16;
+        rgb |= g << 8;
+        rgb |= b;
+
+        return rgb;
     }
 
     function readUInt32(bytes memory buffer, uint position) internal pure returns (uint32, uint) {
