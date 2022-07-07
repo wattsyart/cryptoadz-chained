a0aa9aafeac888c071ec69b01fbafa081f48aab5
Wattsy <wattsyart@protonmail.com>
2022-07-07 00:47:49 -0400

    fix collector script

--- scripts/collect.js
+++ scripts/collect.js
@@ -35,7 +35,19 @@ async function main() {
 }
 
 async function deploy() {
-  const CrypToadz = await ethers.getContractFactory('CrypToadz');
+  const GIFEncoder = await ethers.getContractFactory("GIFEncoder");
+  var GIFEncoderDeployed = await GIFEncoder.deploy();
+  await GIFEncoderDeployed.deployed();
+
+  const PixelRenderer = await ethers.getContractFactory("PixelRenderer");
+  var PixelRendererDeployed = await PixelRenderer.deploy();
+  await PixelRendererDeployed.deployed();
+
+  const CrypToadz = await ethers.getContractFactory("CrypToadz",  { libraries: {
+      "GIFEncoder": GIFEncoderDeployed.address,
+      "PixelRenderer": PixelRendererDeployed.address
+  }});
+  
   const toadz = await CrypToadz.deploy();
   await toadz.deployed();
   console.log(gutil.colors.blue(`CrypToadz deployed to: '${toadz.address}'`));
