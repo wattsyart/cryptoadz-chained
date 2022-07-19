cd533c1562179f8311425095c6124b057c2b8b1a
Wattsy <wattsyart@protonmail.com>
2022-07-19 02:19:03 -0400

    nit: warnings

--- contracts/Errors.sol
+++ contracts/Errors.sol
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: MPL-2.0
+
+pragma solidity ^0.8.13;
+
 error UnsupportedDrawInstruction(uint8 instructionType);
 error DoNotAddBlackToColorTable();
 error InvalidDrawOrder(uint8 featureId);
