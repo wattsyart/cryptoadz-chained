204593619dc7f1f06f3de8d9a30b49c9e5143613
Wattsy <wattsyart@protonmail.com>
2022-07-19 02:16:08 -0400

    reinstate basic test

--- test/CrypToadz.js
+++ test/CrypToadz.js
@@ -1,30 +1,48 @@
-// const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-// var toadz;
+var CrypToadzDeployed;
 
-// describe("CrypToadz", function () {
+describe("CrypToadz", function () {
 
-//     beforeEach(async () => {
+    beforeEach(async () => {
 
-//         const GIFEncoder = await ethers.getContractFactory("GIFEncoder");
-//         var GIFEncoderDeployed = await GIFEncoder.deploy();
-//         await GIFEncoderDeployed.deployed();
-
-//         const PixelRenderer = await ethers.getContractFactory("PixelRenderer");
-//         var PixelRendererDeployed = await PixelRenderer.deploy();
-//         await PixelRendererDeployed.deployed();
-
-//         const CrypToadz = await ethers.getContractFactory("CrypToadz",  { libraries: {
-//             "GIFEncoder": GIFEncoderDeployed.address,
-//             "PixelRenderer": PixelRendererDeployed.address
-//         }});
-//         toadz = await CrypToadz.deploy();
-//         await toadz.deployed();
-//     });
+        const GIFEncoder = await ethers.getContractFactory("GIFEncoder");
+        var GIFEncoderDeployed = await GIFEncoder.deploy();
+        await GIFEncoderDeployed.deployed();
     
-//     it("can get metadata for existing token", async function () {
-//         var tokenURI = await toadz.tokenURI(1);
-//         console.log(tokenURI);
-//     });
-// });
+        const PixelRenderer = await ethers.getContractFactory("PixelRenderer");
+        var PixelRendererDeployed = await PixelRenderer.deploy();
+        await PixelRendererDeployed.deployed();
+    
+        const CrypToadzStrings = await ethers.getContractFactory("CrypToadzStrings");
+        var CrypToadzStringsDeployed = await CrypToadzStrings.deploy();
+        await CrypToadzStringsDeployed.deployed();
+    
+        const CrypToadzBuilder = await ethers.getContractFactory("CrypToadzBuilder", {
+          libraries: {
+            PixelRenderer: PixelRendererDeployed.address
+          }
+        });
+        var CrypToadzBuilderDeployed = await CrypToadzBuilder.deploy();
+        await CrypToadzBuilderDeployed.deployed();
+    
+        const CrypToadzMetadata = await ethers.getContractFactory("CrypToadzMetadata");
+        var CrypToadzMetadataDeployed = await CrypToadzMetadata.deploy();
+        await CrypToadzMetadataDeployed.deployed();
+    
+        const CrypToadz = await ethers.getContractFactory("CrypToadz", {
+          libraries: {
+            "GIFEncoder": GIFEncoderDeployed.address,
+            "PixelRenderer": PixelRendererDeployed.address
+          }
+        });
+        var CrypToadzDeployed = await CrypToadz.deploy(CrypToadzStringsDeployed.address, CrypToadzBuilderDeployed.address, CrypToadzMetadataDeployed.address);
+        await CrypToadzDeployed.deployed();
+    });
+    
+    it("can get metadata for existing token", async function () {
+        var tokenURI = await toadz.tokenURI(1);
+        console.log(tokenURI);
+    });
+});
