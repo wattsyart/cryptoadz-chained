ff2405bc2903c8a6deafbc6afa92c9a8ce287a0f
Wattsy <wattsyart@protonmail.com>
2022-07-25 18:06:34 -0400

    cleanup tasks code

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -4,8 +4,8 @@ const utils = require('../scripts/utils.js');
 const fs = require('fs');
 const readline = require('readline');
 
-const gifEncoderAddress = "0x7b62D26EfB24E95334D52dEe696F79D89bb7411F";
-const mainContractAddress = "0x299F288606EeE22364EE19bC0DD97e6Cf65f1b2a";
+const GIFEncoderAddress = "0x7b62D26EfB24E95334D52dEe696F79D89bb7411F";
+const CrypToadzChainedAddress = "0x299F288606EeE22364EE19bC0DD97e6Cf65f1b2a";
 
 task("toadz", "Validates correctness of a single CrypToad")
   .addParam("id", "The CrypToadz token ID to validate")
@@ -14,103 +14,67 @@ task("toadz", "Validates correctness of a single CrypToad")
       var toadz;
       var factory = await ethers.getContractFactory("CrypToadzChained", {
         libraries: {
-          GIFEncoder: gifEncoderAddress
+          GIFEncoder: GIFEncoderAddress
         }
       });
-      toadz = await factory.attach(mainContractAddress);
-      await utils.collect(toadz, parseInt(taskArgs.id), null, true, true);
+      toadz = await factory.attach(CrypToadzChainedAddress);
+
+      var logger = null;
+      await utils.collect(toadz, parseInt(taskArgs.id), logger, true, true);
     });
 
 task("toadz-custom-images", "Validates correctness of a CrypToadz custom images")
   .setAction(
     async (taskArgs) => {
-      var toadz;
-      var factory = await ethers.getContractFactory("CrypToadzChained", {
-        libraries: {
-          GIFEncoder: gifEncoderAddress
-        }
-      });
-      toadz = await factory.attach(mainContractAddress);
-
-      const fileStream = fs.createReadStream('./scripts/customImageIds.txt');
-      const lines = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-      });
-
-      for await (const line of lines) {
-        await utils.collect(toadz, parseInt(line), null, true, true);
-      }
+      checkToadz('./scripts/customImageIds.txt', null, GIFEncoderAddress, CrypToadzChainedAddress, true, true);
     });
 
 task("toadz-custom-animations", "Validates correctness of a CrypToadz animations")
   .setAction(
     async (taskArgs) => {
-      var toadz;
-      var factory = await ethers.getContractFactory("CrypToadzChained", {
-        libraries: {
-          GIFEncoder: gifEncoderAddress
-        }
-      });
-      toadz = await factory.attach(mainContractAddress);
-
-      const fileStream = fs.createReadStream('./scripts/customAnimationIds.txt');
-      const lines = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-      });
-
-      for await (const line of lines) {
-        await utils.collect(toadz, parseInt(line), null, true, true);
-      }
+      checkToadz('./scripts/customAnimationIds.txt', null, GIFEncoderAddress, CrypToadzChainedAddress, true, true);
     });
 
 task("toadz-image-deltas", "Validates correctness of all CrypToadz token images that have deltas")
   .setAction(
     async (taskArgs) => {
-      var toadz;
-      var factory = await ethers.getContractFactory("CrypToadzChained", {
-        libraries: {
-          GIFEncoder: gifEncoderAddress
-        }
-      });
-      toadz = await factory.attach(mainContractAddress);
-
-      const fileStream = fs.createReadStream('./scripts/deltaIds.txt');
-      const lines = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-      });
-
-      for await (const line of lines) {
-        await utils.collect(toadz, parseInt(line), null, false, true);
-      }
+      checkToadz('./scripts/deltaIds.txt', null, GIFEncoderAddress, CrypToadzChainedAddress, false, true);
     });
 
 task("toadz-all", "Validates correctness of all CrypToadz tokens")
   .setAction(
     async (taskArgs) => {
-      var toadz;
+      checkToadz('./scripts/tokenIds.txt', null, GIFEncoderAddress, CrypToadzChainedAddress, true, true);
+    });
+
+task("toadz-all-metadata", "Validates correctness of all CrypToadz token metadata")
+  .setAction(
+    async (taskArgs) => {
+      checkToadz('./scripts/tokenIds.txt', null, GIFEncoderAddress, CrypToadzChainedAddress, true, false);
+    });
+
+task("crush", "Runs pngcrush on all custom images")
+  .setAction(
+    async (taskArgs) => {
+      await utils.crush();
+    });
+
+function checkToadz(idFilePath, gifEncoderAddress, contractAddress, logger, checkMetadata, checkImage) {
+  var toadz;
       var factory = await ethers.getContractFactory("CrypToadzChained", {
         libraries: {
           GIFEncoder: gifEncoderAddress
         }
       });
-      toadz = await factory.attach(mainContractAddress);
+      toadz = await factory.attach(contractAddress);
 
-      const fileStream = fs.createReadStream('./scripts/tokenIds.txt');
+      const fileStream = fs.createReadStream(idFilePath);
       const lines = readline.createInterface({
         input: fileStream,
         crlfDelay: Infinity
       });
 
       for await (const line of lines) {
-        await utils.collect(toadz, parseInt(line), null, true, true);
+        await utils.collect(toadz, parseInt(line), logger, checkMetadata, checkImage);
       }
-    });
-
-task("crush", "Runs pngcrush on all custom images")
-  .setAction(
-    async (taskArgs) => {      
-      await utils.crush();
-    });
+}
