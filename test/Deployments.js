ce31822b15379f1fc616ffa295f9617fa33a0c47
Wattsy <wattsyart@protonmail.com>
2022-08-07 12:48:18 -0400

    nit: comments for deploy test

--- test/Deployments.js
+++ test/Deployments.js
@@ -4,12 +4,23 @@ const utils = require('../scripts/deploy.js');
 describe("Deployments", function () {
   it("deploys all contracts", async function () {
     const network = await ethers.provider.getNetwork();
-    console.log(`Network: ${network.chainId}`);
-    if(network.chainId !== 8134646) {
-      await utils.deployContracts(hre.ethers, false, false, getTxOptions());
-    } else {
+    console.log(`network: ${network.chainId}`);    
+
+    // private node has throw-away key, doesn't need a hardware wallet
+    if(network.chainId == 8134646) {      
       await utils.deployContracts(hre.ethers, false, false);
+      return;
     }
+
+    // Deploy to real network w/ hardware wallet HID:
+    await utils.deployContracts(hre.ethers, false, false, getTxOptions(), "HID_FRESH_ADDRESS_PATH_GOES_HERE");
+
+    // Random Testing:
+    /*
+    var output = await utils.deployRandomContracts(hre.ethers, false, false);
+    var tokenURI = await output["CrypToadzChained"].randomTokenURIFromSeed("2439799993720087534");
+    console.log(tokenURI);
+    */
   });
 });
 
