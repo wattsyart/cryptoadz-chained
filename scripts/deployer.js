a0913fec3ee767cacf964b747c227a3a7b63bf89
Wattsy <wattsyart@protonmail.com>
2022-08-07 16:37:46 -0400

    nit: cleanup deployer.js as it was neglected

--- scripts/deployer.js
+++ scripts/deployer.js
@@ -1,7 +1,27 @@
 const utils = require('./deploy.js');
+const hre = require("hardhat");
 
 async function main() {
-  await utils.deployContracts();
+  const network = await ethers.provider.getNetwork();
+
+  // private node has throw-away key, doesn't need a hardware wallet
+  if(network.chainId == 8134646) {      
+    await utils.deployContracts(hre.ethers, false, false);
+    return;
+  }
+
+  // Deploy to real network (using HID or adding a local signer is left to the caller to override):
+  await utils.deployContracts(hre.ethers, false, false, getTxOptions());
+}
+
+function getTxOptions() {
+  var baseFeePerGas = ethers.BigNumber.from(4).mul(hre.ethers.BigNumber.from(1000000000));
+  var priorityFeeInWei = ethers.BigNumber.from(2).mul(hre.ethers.BigNumber.from(1000000000));
+  const txOptions = {        
+      maxPriorityFeePerGas: priorityFeeInWei,
+      maxFeePerGas: baseFeePerGas.add(priorityFeeInWei)        
+  }
+  return txOptions;
 }
 
 main()
