5375e344a8318be1940b549127ec9677a56493c5
Wattsy <wattsyart@protonmail.com>
2022-08-08 01:22:29 -0400

    bind MainNetRpcUrl to settings

--- ui/CrypToadzChained/CrypToadzChained/Shared/Web3Options.cs
+++ ui/CrypToadzChained/CrypToadzChained/Shared/Web3Options.cs
@@ -5,6 +5,7 @@ namespace CrypToadzChained.Shared
     public sealed class Web3Options
     {
         [Required] public string RpcUrl { get; set; } = null!;
+        [Required] public string MainNetRpcUrl { get; set; } = null!;
         [Required] public string IpfsUrl { get; set; } = null!;
         [Required] public string ContractAddress { get; set; } = null!;
     }
