3b55925cbef821b7c127fc098a04778059f28e97
Wattsy <wattsyart@protonmail.com>
2022-08-09 18:38:10 -0400

    document client-side settings

--- ui/CrypToadzChained/CrypToadzChained/Client/Program.cs
+++ ui/CrypToadzChained/CrypToadzChained/Client/Program.cs
@@ -10,11 +10,23 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazorTable();
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<ParityService>();
 
 builder.Services.Configure<Web3Options>(o =>
 {
+    // contract address for the on-chain project
     o.ContractAddress = "0x4190aC5bc2499dC0285AC344F92c6E87dF99f93A";
+    
+    // hide URLs in UI for sharing purposes
+    o.HideSensitiveFields = false;
+
+    // RPC URL pointing to the on-chain project
+    o.RpcUrl = null!;
+
+    // RPC URL pointing to mainnet (used in parity testing)
+    o.MainNetRpcUrl = null!;
+    
+    // IPFS gateway URL (used in parity testing)
+    o.IpfsUrl = null!;
 });
 
 await builder.Build().RunAsync();
