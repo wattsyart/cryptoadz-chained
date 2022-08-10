6aabeb830fc33b83483ccfdb29d920913bf63f19
Wattsy <wattsyart@protonmail.com>
2022-08-10 01:53:45 -0400

    improve UI when deltas take longer than the rest (large animations)

---
+++ ui/CrypToadzChained/CrypToadzChained/Shared/ParityScopeExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace CrypToadzChained.Shared;
+
+public static class ParityScopeExtensions
+{
+    public static uint Count(this ParityScope scope)
+    {
+        return (uint) TokenIds(scope).Count();
+    }
+
+    public static IEnumerable<uint> TokenIds(this ParityScope scope)
+    {
+        var tokenIds = scope switch
+        {
+            ParityScope.All => LUT.AllTokenIds,
+            ParityScope.AllGenerated => LUT.AllTokenIds.Except(LUT.CustomImageTokenIds).Except(LUT.CustomAnimationTokenIds),
+            ParityScope.AllCustoms => LUT.CustomImageTokenIds.Concat(LUT.CustomAnimationTokenIds),
+            ParityScope.CustomImages => LUT.CustomImageTokenIds,
+            ParityScope.SmallCustomImages => LUT.CustomImageTokenIds.Except(LUT.LargeImageTokenIds),
+            ParityScope.LargeCustomImages => LUT.LargeImageTokenIds,
+            ParityScope.CustomAnimations => LUT.CustomAnimationTokenIds,
+            ParityScope.LargeCustomAnimations => LUT.LargeAnimationTokenIds,
+            ParityScope.SmallCustomAnimations => LUT.CustomAnimationTokenIds.Except(LUT.LargeAnimationTokenIds),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        return tokenIds;
+    }
+}
--- ui/CrypToadzChained/CrypToadzChained/Shared/ParityService.cs
+++ ui/CrypToadzChained/CrypToadzChained/Shared/ParityService.cs
@@ -53,7 +53,7 @@ namespace CrypToadzChained.Shared
 
             var source = Enum.GetName(typeof(ParitySource), parityOptions.Source)!;
 
-            foreach (var tokenId in GetScopeIds(parityOptions.Scope))
+            foreach (var tokenId in parityOptions.Scope.TokenIds())
             {
                 if (cancellationToken.IsCancellationRequested)
                     return;
@@ -388,30 +388,6 @@ namespace CrypToadzChained.Shared
             if (info.Size == newSize)
                 throw new InvalidOperationException("Image is already the requested size");
             info.Image.Mutate(x => x.Resize(new ResizeOptions { Size = newSize, Sampler = KnownResamplers.NearestNeighbor }));
-        }
-
-        public static uint GetScopeIdCount(ParityScope scope)
-        {
-            return (uint) GetScopeIds(scope).Count();
-        }
-
-        public static IEnumerable<uint> GetScopeIds(ParityScope scope)
-        {
-            var tokenIds = scope switch
-            {
-                ParityScope.All => LUT.AllTokenIds,
-                ParityScope.AllGenerated => LUT.AllTokenIds.Except(LUT.CustomImageTokenIds).Except(LUT.CustomAnimationTokenIds),
-                ParityScope.AllCustoms => LUT.CustomImageTokenIds.Concat(LUT.CustomAnimationTokenIds),
-                ParityScope.CustomImages => LUT.CustomImageTokenIds,
-                ParityScope.SmallCustomImages => LUT.CustomImageTokenIds.Except(LUT.LargeImageTokenIds),
-                ParityScope.LargeCustomImages => LUT.LargeImageTokenIds,
-                ParityScope.CustomAnimations => LUT.CustomAnimationTokenIds,
-                ParityScope.LargeCustomAnimations => LUT.LargeAnimationTokenIds,
-                ParityScope.SmallCustomAnimations => LUT.CustomAnimationTokenIds.Except(LUT.LargeAnimationTokenIds),
-                _ => throw new ArgumentOutOfRangeException()
-            };
-
-            return tokenIds;
         }
 
         #region NodeJS
