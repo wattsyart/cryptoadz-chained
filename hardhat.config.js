77878a6ff5779ca324178ec1c123e6aff32b8d75
Wattsy <wattsyart@protonmail.com>
2022-08-10 17:16:07 -0400

    add stubs for rinkeby and mainnet and etherscan api

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,7 +25,24 @@ require("./tasks/tasks.js");
       gasPrice: "auto",
       gas: "auto",
       timeout: 43200000
+    },
+    rinkeby: {
+      url: "RINKEBY_RPC_URL_GOES_HERE",
+      gasPrice: "auto",
+      gas: "auto",
+      gasMultiplier: 1,
+      timeout: 43200000
+    }, 
+    mainnet: {
+      url: "MAINNET_RPC_URL_GOES_HERE",
+      gasPrice: "auto",
+      gas: "auto",
+      gasMultiplier: 1,
+      timeout: 43200000
     }
+  },
+  etherscan: {
+    apiKey: "ETHERSCAN_API_KEY_GOES_HERE"
   },
   solidity: {
     version: "0.8.13",
