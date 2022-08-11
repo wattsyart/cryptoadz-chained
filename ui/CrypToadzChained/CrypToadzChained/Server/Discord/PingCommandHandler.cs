d6e3b40b9ecdfa3a0af8c96faf8b7f1463aa8f04
Wattsy <wattsyart@protonmail.com>
2022-08-11 15:31:19 -0400

    discord, I guess?

---
+++ ui/CrypToadzChained/CrypToadzChained/Server/Discord/PingCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using TehGM.Discord.Interactions;
+using TehGM.Discord.Interactions.CommandsHandling;
+
+namespace CrypToadzChained.Server.Discord
+{
+    [InteractionCommand(DiscordApplicationCommandType.ChatInput, "ping", "ping")]
+    public class PingCommandHandler : IDiscordInteractionCommandHandler
+    {
+        public Task<DiscordInteractionResponse> InvokeAsync(DiscordInteraction message, HttpRequest request, CancellationToken cancellationToken)
+        {
+            var command = new DiscordInteractionResponseBuilder()
+                .WithText("pong")
+                .Build();
+
+            return Task.FromResult(command);
+        }
+    }
+}
--- ui/CrypToadzChained/CrypToadzChained/Server/Program.cs
+++ ui/CrypToadzChained/CrypToadzChained/Server/Program.cs
@@ -1,10 +1,15 @@
 using CrypToadzChained.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using TehGM.Discord.Interactions;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
-
 builder.Services.Configure<Web3Options>(builder.Configuration.GetSection("Web3"));
+
+builder.Services.AddDiscordInteractions(o => { o.RegisterCommands = true; });
+builder.Services.Configure<DiscordInteractionsOptions>(builder.Configuration.GetSection("Discord"));
 
 
 var app = builder.Build();
@@ -22,6 +27,8 @@ else
 app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
+app.UseDiscordInteractions();
+
 app.UseRouting();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
