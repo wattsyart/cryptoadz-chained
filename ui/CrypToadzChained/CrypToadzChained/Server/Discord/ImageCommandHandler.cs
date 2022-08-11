452f728f8214ecac577adb54e41fa3a25b4b4381
Wattsy <wattsyart@protonmail.com>
2022-08-11 16:19:01 -0400

    forgot to save after file move

--- ui/CrypToadzChained/CrypToadzChained/Server/Discord/ImageCommandHandler.cs
+++ ui/CrypToadzChained/CrypToadzChained/Server/Discord/ImageCommandHandler.cs
@@ -22,23 +22,3 @@ public class ImageCommandHandler : IDiscordInteractionCommandHandler
         return Task.FromResult(command);
     }
 }
-
-// ReSharper disable once UnusedMember.Global (Reflection)
-[InteractionCommand(DiscordApplicationCommandType.ChatInput, "toad", "returns a random, fully on-chain toad")]
-public class ToadCommandHandler : IDiscordInteractionCommandHandler
-{
-    public Task<DiscordInteractionResponse> InvokeAsync(DiscordInteraction message, HttpRequest request, CancellationToken cancellationToken)
-    {
-        var command = new DiscordInteractionResponseBuilder()
-            .AddEmbed(embed =>
-            {
-                var seed = (ulong)new Random().NextInt64();
-                embed.WithType(DiscordEmbedType.Rich);
-                embed.WithThumbnail($"https://cryptoadzchained.com/random/img/{seed}");
-                embed.WithURL($"https://cryptoadzchained.com/random/json/{seed}");
-            })
-            .Build();
-
-        return Task.FromResult(command);
-    }
-}
