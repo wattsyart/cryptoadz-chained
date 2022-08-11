c15656bf5d8b875670818beefede5ea7fa00f093
Wattsy <wattsyart@protonmail.com>
2022-08-11 16:37:25 -0400

    try an embed otherwise nope

--- ui/CrypToadzChained/CrypToadzChained/Server/Discord/ImageCommandHandler.cs
+++ ui/CrypToadzChained/CrypToadzChained/Server/Discord/ImageCommandHandler.cs
@@ -1,11 +1,12 @@
-﻿using TehGM.Discord.Interactions;
+﻿using TehGM.Discord;
+using TehGM.Discord.Interactions;
 using TehGM.Discord.Interactions.CommandsHandling;
 
 namespace CrypToadzChained.Server.Discord;
 
 // ReSharper disable once UnusedMember.Global (Reflection)
-[InteractionCommand(DiscordApplicationCommandType.ChatInput, "toad", "returns a random, fully on-chain toad")]
-public class ToadCommandHandler : IDiscordInteractionCommandHandler
+[InteractionCommand(DiscordApplicationCommandType.ChatInput, "image", "returns a random, fully on-chain toad image")]
+public class ImageCommandHandler : IDiscordInteractionCommandHandler
 {
     public Task<DiscordInteractionResponse> InvokeAsync(DiscordInteraction message, HttpRequest request, CancellationToken cancellationToken)
     {
@@ -13,9 +14,8 @@ public class ToadCommandHandler : IDiscordInteractionCommandHandler
             .AddEmbed(embed =>
             {
                 var seed = (ulong)new Random().NextInt64();
+                embed.WithType(DiscordEmbedType.Image);
                 embed.WithImage($"https://cryptoadzchained.com/random/img/{seed}");
-                embed.WithURL($"https://cryptoadzchained.com/random/{seed}");
-                embed.WithDescription($"CrypToadz #{seed}");
             })
             .Build();
 
