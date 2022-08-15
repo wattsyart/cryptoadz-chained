fa573c412902b186b6ccde945d797e8465b49e16
Wattsy <wattsyart@protonmail.com>
2022-08-15 01:39:57 -0400

    split main custom contracts out (mainly to appease etherscan verify)

---
+++ contracts/generated/CrypToadzCustomImagesSolo.sol
@@ -0,0 +1,360 @@
+// SPDX-License-Identifier: CC0-1.0
+
+/*
+BBBGGBBBBBBGGGBBBGGGGGBBBBBBBBBBB######BBBBBGGGGGGGGGGGGGGBBB#####BBBGGGGGGBBBBBBGGGGGGGGBB#######BB
+GGBBBB##BBBBBBBBBGGBBBBBBBBBBBBBBB####BBBBBBBBBBBBBBBGGGBBB#B#####BBBGGGBBB#####BBBBGGGBBBB###BBBBBB
+GGBBB###BBB##BBBGGGB##BGGGGGBGBBBB##BBBBBBGGBBBBBBBBBBBBBBBBBBBBBBBBBBGGBBBBBB###B#BGGBBBBBBBBBGGBBB
+GGBBB########BGGGGGB##BBBBBBGGBBB###BBBBBBGGGGGGGGBBBBBBBBBBBBBGGGGBBBBBGGGGGB###BBBBBBBBGGGGGGGGB##
+BBBBBBBBBBBBBBBBBBB######BBBGG#@@&#######BGGBB#BBB##BPGBBGGGGGGGGGGGGGBBBBBGGBBBBBBBBBGGGBBBGGBBB###
+BBBBBBBBBBBBBB###BB###BBBBBBBB##&#BBB#BBBBBB##57^:^::::^7GBGGB#BBBBB#BBBBBBBBBBBBBBGGGB####BBBB#B###
+GGGGGGGGGGGGGBBBBB#BBBBBBBBB##BBBBGBBBBGGBBBJ:.^^~~~~~~^:.PBB######P?B##BBBBBBBBBGGGGGBBB##BBBBBB###
+GGGBBBBBGGGGGGBPY????JG##BBB##BBBGG5PBBB5YG^.^~~~~~~~~~~~..777777?!.7GBBG5?!P###BGGBBBBBB##BBBBBBBBB
+GGBBB###BGGGBP!~7Y..!?~~YBGY?7~^GBB^~#BG::~.~~~~~~~~~~~~~.:B:5BGGP5Y?^.^.:?PBBGGGGGBBBBBB##BGGGBBBBB
+BBBBBB##BBBBP.?GBB7 ..~!~^~!7!~7B#B!.7!!7J ^~~~~~~~~~~~~:.PY.B!7#B#G!75GPJ!~^!GBBBBBBB###BBB##BGGBBB
+BBBBBB##BBBB^^BGGGP.::.YG7~!?PBGJ7!7 JBB##~.~~~~^^~~~~^.^P?:P?:G##BBGGP5PGBG7:YBB##BBB###BB###BGGGGG
+BBB###BBBBBB7:GGGGGPBP^^~?GBP?!7?Y557YB#BGG!.:^^..^^::~5P~7B#PG5J!~~~~!!~.PB#Y.PBBBBBB########BBBGGG
+BBBBBBBBBGGGG~^PBBGY!~75G57!77!^::::::^!PP:YG?~~~~~!YG##GPPJ7!~~~~^::!J!:?BBBB.YBGGBBBBBBBBBBBGGGGGG
+BBBBBBBBBBBGGBJ:77^7B@G!^7P?::^~~~~~~~~:.5?.B#BBB###BPJ7~~~~~^:....::^~?G#BB#7.BBBBBBBBBBBBBGGGGGBBB
+GGGGGBBBBBBGGGBGJ5#@&?^JBG:.~~~~~~~~^.^~ ?5~B#BBPY7!~~~~~.....:^^^~!JPB#BBB#J.PBBGG###BBBBBBBBBBB###
+GGGBBB#####BBBBB#&&5.?B#B:.~~~~~~~~~~:^.!BBBPJ!~~~~~~.. :.:^~^~!?5GB#BBBB#G~^PBBGGG###BGB###BBBBB###
+GGBBB######&&&###G^. GBBG ^~~~~~~~~~^:^YP?!~~!!~^.. :.:^^~~!JPBB#BBBBBB#G7^?BBGGGGGGGGGGGBBBBBBBBB##
+BBBBB#######BGGBG.^:^#BG57.:^^^^:::^~!!~~!7!^.. ..^^^~~7YJ!JYYJGBBBB#BG7^?BBBBGGGGGGGGGGG###BB###BGG
+BBBBBB##BBB#J7!~.^! JY5G~~7!!!!?JJ7~~!7!^....:^^^~!?J!G##BP5YY5BB#BPJ~ !BBBBBB#&#GBGGGBBB##BBBBBBGGG
+GGBBBBBBBGG####7.?!:~~ J#PJ?G#BY~~!!^....::^^~7JPGGJ^?BBBBB####G5?!!JG:?BGGGGB&&#BBGGGB####BBBGGGBBB
+GGBBBBBBBBB####~.????^ !!!!PBY:~7:.  .:^~!?5GBB#BBJ7G#BBBBBPY?!!7J5#BG.?BGGGGBBBBBBGGG###BBBBBBGGBBB
+BBBGGGGGBBB##Y^!^:~7?~~!7?::B.^7~.:~7J5GB####BBBBB##BG5J?7!7?5GBB:JP:^ 5BGGBBBBBBGGGGG&&&BBBBBBBBBBB
+###BBBGGB#BBG?5BB57~~:^^~~..5Y?7J5PGGGGGPP555YJJ?!~::::~7YG##B#G:!#G...GBBB##BGGBBBBGGBBBGGGGGBBBBBB
+###B#BBB#&&BGBGGGBB#7.~!~~~~^ 7???^.~: !????JYJ!~~?5G###BY!!5BJ. B#G..:#BB####BBB##BGGBBBBBBGGGGGBBB
+###BBBBB&&&BBBGGBB#!.7?????J7.B#B#~.^:J#BBB#G!7G&@@@@@@@@@@#~.7~.YY7 .J#BBB######B#BGBBB####GBGGGBBB
+BBBBBBBBBBBBBBBBBB7.7?:.^^~~^.GBBB:^YGBBBBBJ:B@#P55##@@B@@@@@!^.:.....GBBBBBBBBBBGGBBBBB#@@&BBBGGB##
+BBBBBBGGBBBBBBBBB5 !??~ 55YYJ.GBBB.5#BBBBBG.#@~!J::7.^^^&@@@@@.7#J ~ 7BBBBBBBBBBBBBB#BBBB###B#BBBB##
+BBBBBBGGGGGBBBBB#~.?77^.B###B.PBBG.5BBBBB#Y:@:7G.J?^^. YG&@@@@~7#Y.~.GBGBBBBBBGGBBBBBBBBB##BBBBBBBBB
+#####BBBGGGBBB###^.~^~!PB57J5.J#BB.J###BPJ~.!^#:7?:~!#5@B#@@@@:J#7..5BBGPPGBBGGGGBBBBBB#BBBBBBBBBBGG
+######BBBGG######5.YB##BBBPY7.^#B#!:P?!!!:^: GG:P~~~@@P^.7P@&!~B#! !BY!!77^JBGGGGGGBBBBBBBBBBBBGGGGG
+######BBBBB#######P^!5BBBBBB#G.5#BB:.7:?##7 YBBB#5 G&@@JGBJ7!YBB#^ ?^!PBBB~:BBBBBBBBGGB########BBBGG
+#####BBBBBBBBBBBBBBBY!~7YGBBB#J.P#BB?!! ^7^Y#BB#P.7 ~@B^?JYY?~GB!^P?.B5^!!.^PB#####BGBB##BBB###BBBBB
+BBBBBBBBBBGGGGBBBBBBB#B5?!!!?5B5:JB##BP5Y5BBBBBY.BGJ.JY??~:7YBP^^PB?.?.5BBBJ:Y#BBBBBBBB#BGGBBBBBBBBB
+BBBBBBBBGGGGGGBBBBBB##BBBBGPY7^?G~^YB#####BBBP~ .Y@G5^&@B:Y#G!^5P777G7P#B5B#J.GGGGGGGGB#BGGGGGGBBBBB
+BBB#####BBBGGGBBBGGB##BBBGGBBBG!^BP!~!J555Y?^ : J.:YP5Y7755~:JBBBBB#B#BJ^:~J^!#BBGGGGGGGGBBBBBBGGGGG
+BB######BGGGGGBBBBBBBBB#BBPJP#BB^~##B.~J???YY:^J~:.~!!7!!7? J#BBBBB#G7^!G#P5G###BGGGBGGGGBBBBBBGGGGG
+BBBBBBBBBGGGGG#####BBBB##B.!.5B#P.GB#~~#BBBBBG7^JGGP5557:77.BBBBB#B?^?GB##BGB###BGGBBBBBBBBBBBBBBBBB
+BBGGGGGGBBBGGG######B#P~?G.Y?.BG!.GB#^!BBBGGGGBG!^5##BB#B5:YBBB#BJ^7P55GY7~ !###BBBBBB######BBBBBBBB
+GGGGGGGGB##BGGBBB###B#B: ^:^B.~^.^BBG.P##BBBBBB##P~~5BBBBBBBBBBJ^7Y!!: ^!!^?BBGGGGGBBBBBBBBBGGBBBBGG
+GGGBBB#####BBBGGB##BBBBB^:: P~.7.GBB~.YYY555PPGB###P~^YBBGBBGY^7GJ:Y? !7..!!7BBBBGGGGGBBBBBB##BBBBBB
+GGBB#BBB####BBBBBBBBBBBBG:!!?B7:PBBJ^5Y!!!!!!77777?YBP!^~!:^!.P&7:B7.5!:!!^^J###BBBGGGBBBBBBBBB#####
+BBBBBBBBB##BBBBBBGGB##BGBP PBY G#BBB5?7^^~!!!!77J55?:J#55BY5#PGJ 5#5GBJ!!?P######B#BGGBBBBBBBBB#####
+BBBBBBBBBBBBBBBBBBBB##BBBB75#G^!JY555YJYJ??77!!~7::7^YBBBGGBBY:?!:GBBBJYB########BBBBBGGGGGGBBB#####
+GGGBBBBBB#####GGGGGGGGGGGBBBGBBG5YJJ?????77!7!!:!JYPGBGBY^~~BPP#G5GGGGBBGGGGGB##BBBBBBGGGBBBBBB#####
+BGBBBBBBBBBBBBGGGBBBBBBBBGGGGGB##########BGGBBBB##BBBGGBJJB?G###BBBGGBBBBBBGGGBBBBBBGGGGGGGGGGBBBBBB
+BBB##BBBBBBGGGBBB########GGGBBBBB######BBBBBB######BBGGGBBB#####B#BGGB##BBBBBBGGGBBGGGBBBBBBGGGGGBBB
+GGGBBBBBBBBGGGBB#######BBGGGBBBBBBBBBBBBBBBBB#####BBBBBGGGB#####BBBBBB##BGGBBBGGGGGGGGBBBBBBGGGGGB##
+*/
+
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@divergencetech/ethier/contracts/utils/DynamicBuffer.sol";
+
+import "../ICrypToadzCustomImages.sol";
+import "../ICrypToadzCustomImageBank.sol";
+import "../BufferUtils.sol";
+import "../CrypToadzCustomImageBank.sol";
+
+contract CrypToadzCustomImages is Ownable, ICrypToadzCustomImages {
+    mapping(uint256 => address) customImageBank;
+
+    function isCustomImage(uint256 tokenId) external view returns (bool) {
+        address bank = customImageBank[tokenId];
+        if (bank == address(0)) {
+            return tokenId == 0x6dbdd5;
+        }
+        return ICrypToadzCustomImageBank(bank).isCustomImage(tokenId);
+    }
+
+    function getCustomImage(uint256 tokenId)
+        external
+        view
+        returns (bytes memory buffer)
+    {
+        if(tokenId == 0x6dbdd5) {
+            return BufferUtils.decompress(customImageData, customImageLength);
+        }
+        return
+            ICrypToadzCustomImageBank(customImageBank[tokenId])
+                .getCustomImage();
+    }
+
+    struct Addresses {
+        address _123;
+        address _316;
+        address _472;
+        address _491;
+        address _703;
+        address _864;
+        address _916;
+        address _936;
+        address _966;
+        address _1005;
+        address _1165;
+        address _1423;
+        address _1559;
+        address _1637;
+        address _1703;
+        address _1754;
+        address _1793;
+        address _1812;
+        address _1935;
+        address _1975;
+        address _2124;
+        address _2232;
+        address _2327;
+        address _2469;
+        address _2471;
+        address _2482;
+        address _2489;
+        address _2521;
+        address _2569;
+        address _2579;
+        address _2709;
+        address _2825;
+        address _2839;
+        address _2846;
+        address _2865;
+        address _2959;
+        address _2986;
+        address _3196;
+        address _3309;
+        address _3382;
+        address _3703;
+        address _4096;
+        address _4126;
+        address _4152;
+        address _4192;
+        address _4201;
+        address _4221;
+        address _4238;
+        address _4368;
+        address _4578;
+        address _4580;
+        address _4604;
+        address _4714;
+        address _4773;
+        address _4845;
+        address _4896;
+        address _5128;
+        address _5150;
+        address _5262;
+        address _5441;
+        address _5471;
+        address _5730;
+        address _5836;
+        address _5848;
+        address _5902;
+        address _6214;
+        address _6382;
+        address _6491;
+        address _6572;
+        address _6578;
+        address _6631;
+        address _6719;
+        address _6736;
+        address _6852;
+        address _6894;
+        address _6916;
+        address _1000000;
+        address _2000000;
+        address _3000000;
+        address _4000000;
+        address _5000000;
+        address _6000000;
+        address _7000000;
+        address _8000000;
+        address _9000000;
+        address _10000000;
+        address _11000000;
+        address _12000000;
+        address _13000000;
+        address _14000000;
+        address _15000000;
+        address _16000000;
+        address _17000000;
+        address _18000000;
+        address _19000000;
+        address _20000000;
+        address _21000000;
+        address _22000000;
+        address _23000000;
+        address _24000000;
+        address _25000000;
+        address _26000000;
+        address _27000000;
+        address _28000000;
+        address _29000000;
+        address _30000000;
+        address _31000000;
+        address _32000000;
+        address _33000000;
+        address _34000000;
+        address _35000000;
+        address _36000000;
+        address _37000000;
+        address _38000000;
+        address _39000000;
+        address _40000000;
+        address _41000000;
+        address _42000000;
+        address _44000000;
+        address _45000000;
+        address _46000000;
+        address _47000000;
+        address _49000000;
+        address _50000000;
+        address _51000000;
+        address _52000000;
+        address _53000000;
+        address _54000000;
+        address _55000000;
+        address _56000000;
+    }
+
+    function setAddresses(Addresses memory a) external onlyOwner {
+        customImageBank[123] = a._123;
+        customImageBank[316] = a._316;
+        customImageBank[472] = a._472;
+        customImageBank[491] = a._491;
+        customImageBank[703] = a._703;
+        customImageBank[864] = a._864;
+        customImageBank[916] = a._916;
+        customImageBank[936] = a._936;
+        customImageBank[966] = a._966;
+        customImageBank[1005] = a._1005;
+        customImageBank[1165] = a._1165;
+        customImageBank[1423] = a._1423;
+        customImageBank[1559] = a._1559;
+        customImageBank[1637] = a._1637;
+        customImageBank[1703] = a._1703;
+        customImageBank[1754] = a._1754;
+        customImageBank[1793] = a._1793;
+        customImageBank[1812] = a._1812;
+        customImageBank[1935] = a._1935;
+        customImageBank[1975] = a._1975;
+        customImageBank[2124] = a._2124;
+        customImageBank[2232] = a._2232;
+        customImageBank[2327] = a._2327;
+        customImageBank[2469] = a._2469;
+        customImageBank[2471] = a._2471;
+        customImageBank[2482] = a._2482;
+        customImageBank[2489] = a._2489;
+        customImageBank[2521] = a._2521;
+        customImageBank[2569] = a._2569;
+        customImageBank[2579] = a._2579;
+        customImageBank[2709] = a._2709;
+        customImageBank[2825] = a._2825;
+        customImageBank[2839] = a._2839;
+        customImageBank[2846] = a._2846;
+        customImageBank[2865] = a._2865;
+        customImageBank[2959] = a._2959;
+        customImageBank[2986] = a._2986;
+        customImageBank[3196] = a._3196;
+        customImageBank[3309] = a._3309;
+        customImageBank[3382] = a._3382;
+        customImageBank[3703] = a._3703;
+        customImageBank[4096] = a._4096;
+        customImageBank[4126] = a._4126;
+        customImageBank[4152] = a._4152;
+        customImageBank[4192] = a._4192;
+        customImageBank[4201] = a._4201;
+        customImageBank[4221] = a._4221;
+        customImageBank[4238] = a._4238;
+        customImageBank[4368] = a._4368;
+        customImageBank[4578] = a._4578;
+        customImageBank[4580] = a._4580;
+        customImageBank[4604] = a._4604;
+        customImageBank[4714] = a._4714;
+        customImageBank[4773] = a._4773;
+        customImageBank[4845] = a._4845;
+        customImageBank[4896] = a._4896;
+        customImageBank[5128] = a._5128;
+        customImageBank[5150] = a._5150;
+        customImageBank[5262] = a._5262;
+        customImageBank[5441] = a._5441;
+        customImageBank[5471] = a._5471;
+        customImageBank[5730] = a._5730;
+        customImageBank[5836] = a._5836;
+        customImageBank[5848] = a._5848;
+        customImageBank[5902] = a._5902;
+        customImageBank[6214] = a._6214;
+        customImageBank[6382] = a._6382;
+        customImageBank[6491] = a._6491;
+        customImageBank[6572] = a._6572;
+        customImageBank[6578] = a._6578;
+        customImageBank[6631] = a._6631;
+        customImageBank[6719] = a._6719;
+        customImageBank[6736] = a._6736;
+        customImageBank[6852] = a._6852;
+        customImageBank[6894] = a._6894;
+        customImageBank[6916] = a._6916;
+        customImageBank[1000000] = a._1000000;
+        customImageBank[2000000] = a._2000000;
+        customImageBank[3000000] = a._3000000;
+        customImageBank[4000000] = a._4000000;
+        customImageBank[5000000] = a._5000000;
+        customImageBank[6000000] = a._6000000;
+        customImageBank[7000000] = a._7000000;
+        customImageBank[8000000] = a._8000000;
+        customImageBank[9000000] = a._9000000;
+        customImageBank[10000000] = a._10000000;
+        customImageBank[11000000] = a._11000000;
+        customImageBank[12000000] = a._12000000;
+        customImageBank[13000000] = a._13000000;
+        customImageBank[14000000] = a._14000000;
+        customImageBank[15000000] = a._15000000;
+        customImageBank[16000000] = a._16000000;
+        customImageBank[17000000] = a._17000000;
+        customImageBank[18000000] = a._18000000;
+        customImageBank[19000000] = a._19000000;
+        customImageBank[20000000] = a._20000000;
+        customImageBank[21000000] = a._21000000;
+        customImageBank[22000000] = a._22000000;
+        customImageBank[23000000] = a._23000000;
+        customImageBank[24000000] = a._24000000;
+        customImageBank[25000000] = a._25000000;
+        customImageBank[26000000] = a._26000000;
+        customImageBank[27000000] = a._27000000;
+        customImageBank[28000000] = a._28000000;
+        customImageBank[29000000] = a._29000000;
+        customImageBank[30000000] = a._30000000;
+        customImageBank[31000000] = a._31000000;
+        customImageBank[32000000] = a._32000000;
+        customImageBank[33000000] = a._33000000;
+        customImageBank[34000000] = a._34000000;
+        customImageBank[35000000] = a._35000000;
+        customImageBank[36000000] = a._36000000;
+        customImageBank[37000000] = a._37000000;
+        customImageBank[38000000] = a._38000000;
+        customImageBank[39000000] = a._39000000;
+        customImageBank[40000000] = a._40000000;
+        customImageBank[41000000] = a._41000000;
+        customImageBank[42000000] = a._42000000;
+        customImageBank[44000000] = a._44000000;
+        customImageBank[45000000] = a._45000000;
+        customImageBank[46000000] = a._46000000;
+        customImageBank[47000000] = a._47000000;
+        customImageBank[49000000] = a._49000000;
+        customImageBank[50000000] = a._50000000;
+        customImageBank[51000000] = a._51000000;
+        customImageBank[52000000] = a._52000000;
+        customImageBank[53000000] = a._53000000;
+        customImageBank[54000000] = a._54000000;
+        customImageBank[55000000] = a._55000000;
+        customImageBank[56000000] = a._56000000;
+    }
+
+    uint16 immutable customImageLength;
+    address immutable customImageData;    
+
+    constructor() {
+        customImageLength = 292;
+        customImageData = SSTORE2.write(hex"eb0cf073e7e592e2626060e0f5f4700902d20e20ccc20c2423dc73de02299674475f4706868dfddc7f1259817cc6e22077278675e7645e0239d2013e21ae26cbb31a25b76f4abc03145896a7737b63c3a5b909ff6faadd3abb9b71eba94b4051ce028fc8620606bec320cc783c7f450a507092a78b6388c7f1b79337f23219f2b8f2a7329e61cb1138f96ba6c28686a73a0d4c6f1c1f6fd4ec0bb859fe56b4f2ebf5dfdb54f54fa718cd94691516cc65eb7851754f30e61bd396ab464c1355ae56adfebace52a8c1fbf9a5d92e4c7f05597fecbc145fe75eb5e7ac5ddab5a3e10e6d8ebb566bf026b3d5f3af58a1e1227a9677c9b20006ad8b523c890d92730e1baf115df8d07fdefc42977ddb9db438aa04c59b80ae63f074f57359e794d00400");        
+    }
+}
