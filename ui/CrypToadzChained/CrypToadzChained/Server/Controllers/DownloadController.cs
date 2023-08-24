e0e631fe4698955aab3585f829b1b89213a00e98
Wattsy <wattsyart@protonmail.com>
2023-08-24 19:49:42 -0400

    Update DownloadController.cs

--- ui/CrypToadzChained/CrypToadzChained/Server/Controllers/DownloadController.cs
+++ ui/CrypToadzChained/CrypToadzChained/Server/Controllers/DownloadController.cs
@@ -252,7 +252,10 @@ public class DownloadController : ControllerBase
     {
         var buffer = Convert.FromBase64String(metadata.Image!.Replace(dataUri, string.Empty));
 
-        var identifier = metadata.Name?.Replace("CrypToadz #", string.Empty).Replace("/", "_");
+        var identifier = metadata.Name?.Replace("CrypToadz #", string.Empty)
+            .Replace("/", "_")
+            .Replace("+", "_")
+            ;
 
         if (ulong.TryParse(identifier, out var tokenId) && tokenId.IsLargeImage())
             return File(buffer, mediaType, DateTimeOffset.Now, ETag(buffer));
